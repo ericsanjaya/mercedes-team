6384818c2c9c0a84d7474426d9d23cc9fd7e4b42
Eric Sanjaya <eric.sanjaya@icloud.com>
2022-05-30 08:02:36 +0800

    Update Car Simulator
    Add Max Speed per Gear, Update Brake logic & Gear Logic

--- src/main/resources/static/js/car.js
+++ src/main/resources/static/js/car.js
@@ -3,40 +3,65 @@ var websocketClient;
 var myVehicleId;
 var maxSpeed = 300;
 var speed = 0;
+var brakeAction = 0;
 var gear = 1;
 var minGear = 1;
 var maxGear = 6;
 
+// Gear max Speed
+var gearMaxSpeed = [0, 30, 50, 80, 160, 230, 300]
+
 var worker;
 
+var nextChangeGear = 0;
+var nextBreak = 0;
 function randomData(vehicleId) {
 
     // gear position
-    var gearAction = Math.floor(Math.random() * 3) -1
-    if (gearAction == 1 && gear < maxGear) {
-        gear += 1;
-    }
-    if (gearAction == -1 && gear > minGear) {
-        gear -= 1;
+    if(Date.now() > nextChangeGear) {
+        var gearAction = Math.floor(Math.random() * 3) -1
+        if (gearAction == 1 && gear < maxGear) {
+            gear += 1;
+            if(speed > gearMaxSpeed[gear]) {
+                speed = gearMaxSpeed[gear];
+            }
+        }
+        if (gearAction == -1 && gear > minGear) {
+            gear -= 1;
+            if(speed > gearMaxSpeed[gear]) {
+                speed = gearMaxSpeed[gear];
+            }
+        }
+
+        var randomNumber = Math.floor(Math.random() * 5);
+        nextChangeGear = Date.now() + (randomNumber * 100);
     }
 
     // speed
     if (speed < maxSpeed) {
-        var newSpeed = speed + gear;
+        var newSpeed = speed + (gear * 3);
         if (newSpeed < maxSpeed) {
             speed = newSpeed;
+            if(speed > gearMaxSpeed[gear]) {
+                speed = gearMaxSpeed[gear];
+            }
         } else {
             speed = maxSpeed;
         }
     }
 
     // brake
-    var brakeAction = Math.floor(Math.random() * 3);
-    var newSpeed = speed - (brakeAction * 3);
+    var newSpeed = speed - (brakeAction);
     if (newSpeed > 0) {
         speed = newSpeed;
     } else {
         speed = 0;
+    }
+
+    if(Date.now() > nextBreak) {
+        brakeAction = Math.floor(Math.random() * 3);
+        var randomNumber = Math.floor(Math.random() * 5);
+        nextBreak = Date.now() + (randomNumber * 50);
     }
 
     document.getElementById("speed").innerHTML  = speed;
